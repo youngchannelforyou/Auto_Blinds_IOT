570d35198fd5d4352c7bbbf10accbcdd6d376e51
uchanyoung <pbl3_t5>
2023-05-23 17:14:54 +0900

    수정본

--- sketch_may22a/sketch_may22a.ino
+++ sketch_may22a/sketch_may22a.ino
@@ -8,13 +8,14 @@ const int TRIG = 3;    // 초음파 송신 핀
 const int ECHO = 2;   // 초음파 수신 핀
 bool Auto = true;
 bool AutoMode = true;
+bool state = false;   // 커튼 상태 false가 올라가있는 상태
 int currentPosition = 0;
 int LIGHT = 0;
 
 void setup() {
   Serial.begin(9600);
-  myStepper.setSpeed(300);
-  UPclockwiseRotation();
+  myStepper.setSpeed(600);
+  // UPclockwiseRotation();
   LIGHT = analogRead(lightSensorPin);
 
   pinMode(TRIG, OUTPUT);
@@ -41,23 +42,34 @@ void loop() {
   if (Auto == true){
     int lightLevel = analogRead(lightSensorPin);
 
-    if(LIGHT > 500 || lightLevel < 400){
-      LIGHT = analogRead(lightSensorPin);
+    Serial.print("LIGHT: ");
+    Serial.print(lightLevel);
+    Serial.print("state: ");
+    Serial.println(state);
+
+    if(state == false && lightLevel <= 30){
       if(AutoMode == true){
         DowncounterClockwiseRotation();
-      } else {
-        UPclockwiseRotation(); 
+        state = true;
       }
-    } else if (LIGHT < 400 || lightLevel > 500){
-      LIGHT = analogRead(lightSensorPin);
-      if(AutoMode == true){
-        UPclockwiseRotation(); 
-      } else {
+    } else if (state == true && lightLevel >= 50 && AutoMode == true){
+      UPclockwiseRotation(); 
+      state = false;
+    }
+    if(state == false && lightLevel >= 50){
+      if(AutoMode == false){
         DowncounterClockwiseRotation();
+        state = true;
+      }
+    } else if (state == true && lightLevel <= 30){
+      if(AutoMode == false){
+        UPclockwiseRotation(); 
+        state = false;
       }
     }
-    
-    delay(500); // 일정 시간 대기
+
+
+  
   }
 }
 
@@ -66,6 +78,10 @@ void UPclockwiseRotation() {
   long duration, distance;
     // 시계 반대 방향으로 한바퀴 회전
   while(true) {  // 64 * 32 = 2048 한바퀴
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println(" cm");
+    
     digitalWrite(TRIG, LOW);
     delayMicroseconds(2);
     digitalWrite(TRIG, HIGH);
@@ -73,14 +89,17 @@ void UPclockwiseRotation() {
     digitalWrite(TRIG, LOW);
     duration = pulseIn (ECHO, HIGH); 
     distance = duration * 17 / 1000; 
-    if (distance > 4){
+    if (distance > 20){
       stopMotor();  // 모터 정지
       currentPosition = 0;
+      state = false;
       return;      // 루프 종료
     }
 
+
+
     currentPosition++;
-    myStepper.step(stepsPerRevolution);
+    myStepper.step(-stepsPerRevolution);
 
     if (Serial.available()) {
       char data = Serial.read();
@@ -97,14 +116,19 @@ void UPclockwiseRotation() {
 void DowncounterClockwiseRotation() {
   long duration, distance;
     // 시계 방향으로 한바퀴 회전
+  if(currentPosition < -65){
+    stopMotor();  // 모터 정지
+    state = true;
+    return;      // 루프 종료
+  }
   while(true) {
-    if(currentPosition < -100){
+    if(currentPosition < -65){
       stopMotor();  // 모터 정지
       return;      // 루프 종료
     }
-
+    state = true;
     currentPosition--;
-    myStepper.step(-stepsPerRevolution);
+    myStepper.step(stepsPerRevolution);
 
     if (Serial.available()) {
       char data = Serial.read();
